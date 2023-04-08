9c9634133f78946b53c48e5e46e938eab63b9496
redhadefinto <redhadefinto28@gmail.com>
2023-04-09 00:15:52 +0700

    route favorite

--- src/routes/index.js
+++ src/routes/index.js
@@ -5,10 +5,12 @@ const welcomeRouter = require("./welcome.route");
 const authRouter = require("./auth.route");
 const productRouter = require("./product.route");
 const profileRouter = require("./profile.route");
+const favoriteRouter = require('./favorite.route')
 
 masterRouter.use("/", welcomeRouter);
 masterRouter.use("/auth", authRouter);
 masterRouter.use("/profile", profileRouter);
 masterRouter.use("/product", productRouter);
+masterRouter.use("/favorite", favoriteRouter)
 
 module.exports = masterRouter;
