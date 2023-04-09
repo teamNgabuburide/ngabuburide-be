053d6208a9d21aa8d60b537cb9713f28f6f506f5
redhadefinto <redhadefinto28@gmail.com>
2023-04-09 22:04:57 +0700

    update: response favorite

--- src/models/favorite.model.js
+++ src/models/favorite.model.js
@@ -4,13 +4,13 @@ const getFavorite = (id) => {
   return new Promise((resolve, reject) => {
     const sqlQuery = `SELECT f.user_id, pi.image, f.prod_id, p.prod_name, p.stock, p.price 
     FROM favorite f 
-    JOIN users u ON u.id = $1
+    JOIN users u ON u.id = f.user_id 
     JOIN products p ON p.id = f.prod_id 
     JOIN (
       SELECT prod_id, MIN(image) AS image
       FROM prod_images
       GROUP BY prod_id
-    ) pi ON p.id = pi.prod_id;`;
+    ) pi ON p.id = pi.prod_id where f.user_id = $1;`;
     db.query(sqlQuery, [id], (err, result) => {
       if (err) {
         reject(err);
