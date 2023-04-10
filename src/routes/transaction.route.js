b0521ca738fffef02fc38332795f9b2a39b76c72
Abdul Rosid <rosid.dev@yahoo.com>
2023-04-11 00:42:53 +0700

    add: code to get data transaction by user id

--- src/routes/transaction.route.js
+++ src/routes/transaction.route.js
@@ -3,5 +3,6 @@ const route = express.Router();
 const transactionController = require("../controllers/transaction.controller");
 
 route.post("/add", transactionController.addTransaction);
+route.get("/:id", transactionController.getTransactionByUser);
 
 module.exports = route;
